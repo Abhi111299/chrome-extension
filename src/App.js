61e858017299baa38310c1f9b5ec1cb78839953c
unknown <abhishekyadav9992183177@gmail.com>
2024-05-02 08:16:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>Airbnb</h4>
+        <p>
+        Airbnb was born in 2007 when two Hosts welcomed three guests to their San Francisco home, and has since grown to over 5 million Hosts who have welcomed over 1.5 billion guest arrivals in almost every country across the globe. Every day, Hosts offer unique stays and experiences that make it possible for guests to connect with communities in a more authentic way.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
